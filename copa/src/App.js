158485ac2e96523c1f6d7c0c985c7ae75f79cd8f
izabela-pereira <izabela@unochapeco.edu.br>
2022-09-23 15:02:53 -0300

    Novo Projeto Copa

--- copa/src/App.js
+++ copa/src/App.js
@@ -3,13 +3,13 @@ import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-      <MenuPageTemplate>
-        <Routes>
-          <Route path ="/" exact = {true} element = {<HomePage/>}/>
-          <Route path ="/login" exact = {true} element = {<LoginPage/>}/>
-          <Route path ="/dados" exact = {true} element = {<DadosPage/>}/>
-        </Routes>
-    </MenuPageTemplate>
+      // <MenuPageTemplate>
+      //   <Routes>
+      //     <Route path ="/" exact = {true} element = {<HomePage/>}/>
+      //     <Route path ="/login" exact = {true} element = {<LoginPage/>}/>
+      //     <Route path ="/dados" exact = {true} element = {<DadosPage/>}/>
+      //   </Routes>
+      // </MenuPageTemplate>
   );
 }
 
